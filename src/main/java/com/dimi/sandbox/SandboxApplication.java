bfcbc67bb533e019b236e684b746aa9fa1e252ce
amustapha <rashrayhan@gmail.com>
2022-12-12 09:05:30 -0600

    first commit

---
+++ src/main/java/com/dimi/sandbox/SandboxApplication.java
@@ -0,0 +1,20 @@
+package com.dimi.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxApplication.class, args);
+	}
+
+	boolean lengthEquals0(String string) {
+		return string.length() == 0;
+	}
+
+	boolean optimizedMethod(String string) {
+		return string.isEmpty();
+	}
+}
